99599c73e73b86620f6e79fc3aecc744eb870d2c
Danillo Souza <danillosouza1704@gmail.com>
2022-02-19 16:44:19 -0300

    Adicionando trinagulação e parametrização
    Co-Authored-by: Daniel Primo <danielprimo9@gmail.com>

--- test/IrpfTest.java
+++ test/IrpfTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IrpfTest {
@@ -27,10 +29,16 @@ class IrpfTest {
         assertEquals(value, irpf.getRendimentos().get(0).getValue());
     }
 
-    @Test
-    void getRendimentoTotal() {
-        irpf.addRendimento("Salario Bonus", 500);
-        irpf.addRendimento("Salario", 5000);
-        assertEquals(5500, irpf.getRendimentoTotal());
+    @ParameterizedTest
+    @CsvSource({
+            "1000, 2000, 321, 3321",
+            "6000, 5000, 3000, 14000",
+            "8000, 2681, 3920, 14601"
+    })
+    void getRendimentoTotal(int value1, int value2, int value3, int finalValue) {
+        irpf.addRendimento("Salario", value1);
+        irpf.addRendimento("Salario", value2);
+        irpf.addRendimento("Salario", value3);
+        assertEquals(finalValue, irpf.getRendimentoTotal());
     }
 }
